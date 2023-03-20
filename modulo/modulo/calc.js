d0a4c67fd7a01b9d0e388a4a7e631b4fb3ee837b
Bianca Gomes <100931395+projbiancagomes@users.noreply.github.com>
2023-03-20 19:58:05 -0300

    21/03/2023

---
+++ modulo/modulo/calc.js
@@ -0,0 +1,10 @@
+const somaF = require('./soma')
+const divF = require('./div')
+const multF = require('./mult')
+const subF = require('./sub')
+
+console.log("A soma é: " + somaF(3,3))
+console.log("A divisão é:" + divF(3,3))
+console.log("A multiplicação é:" + multF(3,3))
+console.log("A subtração é:" + subF(3,3))
+
---
+++ modulo/modulo/soma.js
@@ -0,0 +1,6 @@
+const soma = function(a,b){
+    return a+b
+}
+
+module.exports = soma
+
