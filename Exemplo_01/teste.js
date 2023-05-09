3c35af7f93f46fdb42869154d268b24d60d0eaa9
Bianca Gomes <100931395+projbiancagomes@users.noreply.github.com>
2023-05-09 10:49:39 -0300

    09/05/2023

---
+++ Exemplo_01/teste.js
@@ -0,0 +1,32 @@
+//var nome = prompt("Digite seu nome: ")
+//console.log(nome)
+
+//alert("Bem vindo " + nome)
+
+/*
+var a = 15
+if (true){
+    let a = 10
+    console.log(a)
+}
+console.log(a)
+*/
+
+
+//Calculando a soma de 2 números
+
+var n1 = parseInt(prompt("Digite N1: ")) 
+var n2 = parseInt(prompt("Digite N2: ")) 
+
+var soma = n1 + n2
+
+//console.log("A soma é " + soma)
+
+//template strin
+//console.log(`A soma é ${soma}  `)
+document.write(`<p>A soma é ${soma}</p>`)
+
+
+
+
+//console.log(typeof n1)
